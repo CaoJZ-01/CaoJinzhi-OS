306c7262aa3892a9776acf85ca1946396f21929b
CaoJZ-01 <68212970+CaoJZ-01@users.noreply.github.com>
2021-04-22 16:22:09 +0800

    页表实验

--- os/src/main.rs
+++ os/src/main.rs
@@ -32,25 +32,19 @@ mod interrupt;
 mod memory;
 extern crate alloc;
 
-/// Rust 入口函数
+/// Rust 的⼊⼝函数
+///
+/// 在 `_start` 为我们进⾏了⼀系列准备之后，这是第⼀个被调⽤的 Rust 函数
 #[no_mangle]
 pub extern "C" fn rust_main() -> ! {
-    // 初始化
-    interrupt::init();
-    memory::init();
+	// 初始化各种模块
+	interrupt::init();
+	memory::init();
 
-    // 物理页分配
-    for _ in 0..2 {
-        let frame_0 = match memory::frame::FRAME_ALLOCATOR.lock().alloc() {
-            Result::Ok(frame_tracker) => frame_tracker,
-            Result::Err(err) => panic!("{}", err)
-        };
-        let frame_1 = match memory::frame::FRAME_ALLOCATOR.lock().alloc() {
-            Result::Ok(frame_tracker) => frame_tracker,
-            Result::Err(err) => panic!("{}", err)
-        };
-        println!("{} and {}", frame_0.address(), frame_1.address());
-    }
+	let remap = memory::mapping::MemorySet::new_kernel().unwrap();
+	remap.activate();
 
-    panic!("Process ended. System shuting down.")
+	println!("kernel remapped");
+
+	panic!()
 }
